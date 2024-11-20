c2690e43128517fdb211cbd45afc2f843d777792
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-11-20 19:22:47 +0700

    ubah detail peminjaman invetaris

--- resources/views/Laboran/peminjaman_inventaris/detail.blade.php
+++ resources/views/Laboran/peminjaman_inventaris/detail.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.laboran.app', [
-    'title' => 'Detail Data PeminjamanInventaris',
+    'title' => 'Detail Data Peminjaman Inventaris',
 ])
+
 @section('konten')
     <div class="row">
         <div class="col d-flex justify-content-center">
             <div class="card mt-4 mb-5" style="width: 800px">
                 <div class="card-body">
-                    <h5 class="card-title text-center">Detail Data
-                        User</h5>
+                    <h5 class="card-title text-center">Detail Data Peminjaman Inventaris</h5>
                     <table class="table" style="border: 0;">
                         <tbody>
                             <tr>
@@ -15,7 +15,7 @@
                                 <td>{{ $data->username }}</td>
                             </tr>
                             <tr>
-                                <th>Nim</th>
+                                <th>NIM</th>
                                 <td>{{ $data->nim }}</td>
                             </tr>
                             <tr>
@@ -27,23 +27,41 @@
                                 <td>{{ $data->semester }}</td>
                             </tr>
                             <tr>
-                                <th>No.HP</th>
-                                <td>{{ $data->no_hp }}</td>
-                            </tr>
-                            <tr>
-                                <th>Email</th>
-                                <td>{{ $data->email }}</td>
-                            </tr>
-                            <tr>
                                 <th>Nama Lab</th>
                                 <td>{{ $data->nama_lab }}</td>
                             </tr>
                             <tr>
-                                <th>Level</th>
-                                <td>{{ $data->level }}</td>
+                                <th>Nama Barang</th>
+                                <td>{{ $data->nama_barang }}</td>
+                            </tr>
+                            <tr>
+                                <th>Tanggal Pinjam</th>
+                                <td>{{ $data->tanggal_pinjam }}</td>
+                            </tr>
+                            <tr>
+                                <th>Tanggal Kembali</th>
+                                <td>{{ $data->tanggal_kembali }}</td>
+                            </tr>
+                            <tr>
+                                <th>Jam Pinjam</th>
+                                <td>{{ $data->jam_pinjam }}</td>
+                            </tr>
+                            <tr>
+                                <th>Jam Kembali</th>
+                                <td>{{ $data->jam_kembali }}</td>
+                            </tr>
+                            <tr>
+                                <th>Keperluan</th>
+                                <td>{{ $data->keperluan }}</td>
+                            </tr>
+                            <tr>
+                                <th>Status</th>
+                                <td>{{ $data->status }}</td>
                             </tr>
                         </tbody>
                     </table>
-
                 </div>
-            @endsection
+            </div>
+        </div>
+    </div>
+@endsection
