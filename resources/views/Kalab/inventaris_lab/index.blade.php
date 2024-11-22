5b00d8dccd32244a05c9439573be2b916b057445
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-11-22 18:28:26 +0700

    add halaman index inventaris lab di aktor kalab

---
+++ resources/views/Kalab/inventaris_lab/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.kalab.app', [
+    'title' => 'Data inventaris Lab',
+])
+@section('konten')
+    @if (Session::get('failed'))
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Login Gagal!</strong> {{ Session::get('failed') }}
+            <button type="button" class="btn-close" data-bs- dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+    @if (Session::get('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Berhasil!</strong> {{ Session::get('success') }}
+        </div>
+    @endif
+    <h3>Data Inventaris lab </h3>
+    <br>
+    <a class="btn btn-outline-success" href="{{ route('laboran.inventaris_lab.tambah') }}">Tambah Data</a>
+    <div>
+        <table class="table" style="margin-top: 10px">
+            <thead>
+                <tr>
+                    <th scope="col">No</th>
+                    <th scope="col">Nama Barang</th>
+                    <th scope="col">Kode Barang</th>
+                    <th scope="col">Kategori</th>
+                    <th scope="col">Jumlah</th>
+                    <th scope="col">Kondisi</th>
+                    <th scope="col">Aksi</th>
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @php $no = 1; @endphp
+                @foreach ($data as $item)
+                    <!-- menampilkan mahasiswa saja -->
+                    @if ($item->nama_lab == Auth::user()->nama_lab)
+                        <tr>
+                            <td class="">{{ $no++ }}</td>
+                            <td>{{ $item->nama_barang }}</td>
+                            <td>{{ $item->nama_lab }}</td>
+                            <td>{{ $item->kode_barang }}</td>
+                            <td>{{ $item->kategori }}</td>
+                            <td>{{ $item->jumlah }}</td>
+                            <td>{{ $item->kondisi }}</td>
+                            <td>
+                                <a class="btn btn-outline-success" href="/editUser/">Detail</a>
+                                <a class="btn btn-outline-warning" href="/editUser/">Edit</a>
+                                <a class="btn btn-outline-danger" href="/deleteUser/">Delete</a>
+                            </td>
+                        </tr>
+                    @endif
+                @endforeach
+            </tbody>
+        </table><br>
+
+    </div>
+
+    <!-- Content Row -->
+    </div>
+    </div>
+@endsection
