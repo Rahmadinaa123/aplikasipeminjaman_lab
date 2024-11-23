16c3aaa8dc109997234c35f1adf95b784845c1ad
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-11-23 20:59:55 +0700

    create controllers mahasiswa peminjaman inventaris lab

---
+++ app/Http/Controllers/MahasiswaPeminjamanInventarisLabController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\MahasiswaPeminjamanInventarisLab;
+use Illuminate\Http\Request;
+
+class MahasiswaPeminjamanInventarisLabController extends Controller
+{
+     //halaman Peminjaman Inventaris 
+    public function index() {
+        $data=peminjamanInventarisLab::all();
+        return view('Mahasiswa.peminjamaninventaris.Index', compact('data'));
+   }
+}
