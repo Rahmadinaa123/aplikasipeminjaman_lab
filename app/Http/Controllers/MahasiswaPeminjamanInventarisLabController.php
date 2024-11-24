882717efa9f16d4c6bbba698d449faf37856c92d
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-11-25 00:36:11 +0700

    create function postpeminjaman Inventaris lab pada mahasiswa

--- app/Http/Controllers/MahasiswaPeminjamanInventarisLabController.php
+++ app/Http/Controllers/MahasiswaPeminjamanInventarisLabController.php
@@ -11,4 +11,51 @@ class MahasiswaPeminjamanInventarisLabController extends Controller
         $labName = $request->query('lab'); // Menangkap parameter lab dari URL
         return view('Mahasiswa.peminjamaninventaris', compact('labName'));
    }
+   
+    public function postPeminjamanInventarisLab(Request $request) {
+    // Validasi input dari form
+    $request->validate([
+        'username' => 'required',              // Nama peminjam
+        'id_user'=> 'required',                
+        'nim' => 'required',
+        'prodi' => 'required',
+        'semester' => 'required|integer|min:1|max:14',
+        'nama_lab' => 'required',
+        'tanggal_pinjam' => 'required|date',    // Tanggal mulai peminjaman
+        'tanggal_kembali' => 'required|date',  // Tanggal selesai peminjaman
+        'jam_pinjam' => 'required|date_format:H:i',    // Jam mulai pemakaian
+        'jam_kembali' => 'required|date_format:H:i',  // Jam selesai pemakaian
+        'keperluan' => 'required',
+    ]);
+
+    dd($request->all());
+
+    // Membuat instance baru dari model Peminjaman
+    $peminjamaninventarislab = new peminjamanInventarisLab;
+
+    // Mengambil data dari form dan menyimpannya ke dalam variabel
+    $peminjamaninventarislab->id_user = $request->id_user; // Menambahkan id_user
+    $peminjamaninventarislab->username = $request->username; // Menggunakan username sebagai nama peminjam
+    $peminjamaninventarislab->nim = $request->nim;
+    $peminjamaninventarislab->prodi = $request->prodi;
+    $peminjamaninventarislab->semester = $request->semester;
+    $peminjamaninventarislab->nama_lab = $request->nama_lab;
+    $peminjamaninventarislab->tanggal_mulai = $request->tanggal_pinjam; // Sesuaikan dengan nama form
+    $peminjamaninventarislab->tanggal_selesai = $request->tanggal_kembali; // Sesuaikan dengan nama form
+    $peminjamaninventarislab->jam_mulai = $request->jam_pinjam; // Sesuaikan dengan nama form
+    $peminjamaninventarislab->jam_selesai = $request->jam_kembali; // Sesuaikan dengan nama form
+    $peminjamaninventarislab->keperluan = $request->keperluan;
+
+    // Menyimpan data ke database
+    $peminjamaninventarislab->save();
+
+    // Mengecek apakah proses simpan berhasil
+    if ($peminjamaninventarislab) {
+        return redirect('/mahasiswa/daftarLab')->with('success', 'Data berhasil disimpan!');
+    } else {
+        return back()->with('failed', 'Maaf, terjadi kesalahan, coba kembali beberapa saat!');
+    }
+}
+
+
 }
