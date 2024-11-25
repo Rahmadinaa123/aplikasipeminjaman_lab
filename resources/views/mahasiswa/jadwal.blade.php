b9ac2cf43a2fb9a3caac0d77818fd08d0322d337
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-11-25 22:45:15 +0700

    update blade jadwal pada mahasiswa

--- resources/views/mahasiswa/jadwal.blade.php
+++ resources/views/mahasiswa/jadwal.blade.php
@@ -15,17 +15,17 @@
             <select name="lab" id="lab" class="form-control" onchange="this.form.submit()">
                 <option value="">All Labs</option>
                 @foreach($labs as $lab)
-                    <option value="{{ $lab }}" {{ $selectedLab == $lab ? 'selected' : '' }}>
-                        {{ $lab }}
-                    </option>
+                <option value="{{ $lab }}" {{ $selectedLab == $lab ? 'selected' : '' }}>
+                    {{ $lab }}
+                </option>
                 @endforeach
             </select>
         </div>
     </form>
 
     <!-- Schedule Table -->
-    <table class="table table-bordered">
-        <thead class="thead-light">
+    <table class="table table-bordered table-striped table-hover">
+        <thead class="table-primary">
             <tr>
                 <th>Day</th>
                 <th>Hour</th>
@@ -38,23 +38,23 @@
         <tbody>
             @php $currentDay = '' @endphp
             @forelse($jadwal as $item)
-                <tr>
-                    @if ($currentDay != $item->hari)
-                        <td>{{ ucfirst($item->hari) }}</td>
-                        @php $currentDay = $item->hari @endphp
-                    @else
-                        <td></td>
-                    @endif
-                    <td>{{ $item->jam_mulai }} - {{ $item->jam_selesai }}</td>
-                    <td>{{ $item->kelas }}</td>
-                    <td>{{ $item->mata_kuliah }}</td>
-                    <td>{{ $item->dosen }}</td>
-                    <td>{{ $item->nama_lab}}</td>
-                </tr>
+            <tr>
+                @if ($currentDay != $item->hari)
+                <td>{{ ucfirst($item->hari) }}</td>
+                @php $currentDay = $item->hari @endphp
+                @else
+                <td></td>
+                @endif
+                <td>{{ $item->jam_mulai }} - {{ $item->jam_selesai }}</td>
+                <td>{{ $item->kelas }}</td>
+                <td>{{ $item->mata_kuliah }}</td>
+                <td>{{ $item->dosen }}</td>
+                <td>{{ $item->nama_lab}}</td>
+            </tr>
             @empty
-                <tr>
-                    <td colspan="6" class="text-center">No data available for the selected lab</td>
-                </tr>
+            <tr>
+                <td colspan="6" class="text-center">No data available for the selected lab</td>
+            </tr>
             @endforelse
         </tbody>
     </table>
