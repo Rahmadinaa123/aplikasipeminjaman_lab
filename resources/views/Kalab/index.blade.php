2aff2997e1e996de4d8e2d5f01a830b5235b0a0e
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2024-12-21 20:05:21 +0700

    index kalab dashboard

--- resources/views/Kalab/index.blade.php
+++ resources/views/Kalab/index.blade.php
@@ -11,7 +11,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                        <a href="{{ route('laboran.peminjaman_inventaris') }}"
+                            <a href="{{ route('laboran.peminjaman_inventaris') }}"
                                 class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Data Peminjaman Lab</a>
                             <h4>{{ number_format($peminjaman) }}</h4>
@@ -32,8 +32,7 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                        <a href="#"
-                                class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            <a href="#" class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Data Jadwal Lab</a>
                             <h4>{{ number_format($jadwal) }}</h4>
                             <div class="h5 mb-0 font-weight-bold text-gray-800">
@@ -52,10 +51,10 @@
                 <div class="card-body">
                     <div class="row no-gutters align-items-center">
                         <div class="col mr-2">
-                        <a href="#"
-                                class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                            <a href="#" class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                 Data Inventaris Lab</a>
                             <h4>{{ number_format($inventaris) }}</h4>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">
                             </div>
                         </div>
                         <div class="col-auto">
