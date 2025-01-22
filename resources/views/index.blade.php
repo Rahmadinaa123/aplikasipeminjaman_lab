9b096b479de8f25ccd2ee38e1b64fe0924f909d8
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2025-01-22 21:35:05 +0700

    tes rubah index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,8 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
 
+    <!-- tes -->
+
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
     <link href="https://fonts.gstatic.com" rel="preconnect" crossorigin>
@@ -149,35 +151,35 @@
 
                 <div class="swiper init-swiper">
                     <script type="application/json" class="swiper-config">
-            {
-              "loop": true,
-              "speed": 600,
-              "autoplay": {
-                "delay": 5000
-              },
-              "slidesPerView": "auto",
-              "centeredSlides": true,
-              "pagination": {
-                "el": ".swiper-pagination",
-                "type": "bullets",
-                "clickable": true
-              },
-              "breakpoints": {
-                "320": {
-                  "slidesPerView": 1,
-                  "spaceBetween": 0
-                },
-                "768": {
-                  "slidesPerView": 3,
-                  "spaceBetween": 20
-                },
-                "1200": {
-                  "slidesPerView": 5,
-                  "spaceBetween": 20
-                }
-              }
-            }
-          </script>
+                    {
+                        "loop": true,
+                        "speed": 600,
+                        "autoplay": {
+                            "delay": 5000
+                        },
+                        "slidesPerView": "auto",
+                        "centeredSlides": true,
+                        "pagination": {
+                            "el": ".swiper-pagination",
+                            "type": "bullets",
+                            "clickable": true
+                        },
+                        "breakpoints": {
+                            "320": {
+                                "slidesPerView": 1,
+                                "spaceBetween": 0
+                            },
+                            "768": {
+                                "slidesPerView": 3,
+                                "spaceBetween": 20
+                            },
+                            "1200": {
+                                "slidesPerView": 5,
+                                "spaceBetween": 20
+                            }
+                        }
+                    }
+                    </script>
                     <div class="swiper-wrapper align-items-center">
                         <div class="swiper-slide"><a class="glightbox" data-gallery="images-gallery"
                                 href="assets/img/gallery/lab1.jpg"><img src="assets/img/gallery/lab1.jpg"
