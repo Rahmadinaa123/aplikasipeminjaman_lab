0f3a7ec9a0771ca99c5a842958ec703191c088e3
Rahmadinaa123 <rahmadinaa2002@gmail.com>
2025-01-22 21:52:05 +0700

    tes perubahan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="keywords" content="">
 
-    <!-- tes server perubahan e server -->
+    <!-- tes server perubahan ke server -->
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com" rel="preconnect">
